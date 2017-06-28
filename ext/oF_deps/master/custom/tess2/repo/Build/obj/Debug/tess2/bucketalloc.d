9d280e8947c4baa7179d13d2e0ea2c8fd2bf8888
Raven <RavensKrag@gmail.com>
2017-06-28 13:06:38 -0400

    Remove tess2 build files (autogenerated by premake4)

--- ext/oF_deps/master/custom/tess2/repo/Build/obj/Debug/tess2/bucketalloc.d
+++
@@ -1,4 +0,0 @@
-obj/Debug/tess2/bucketalloc.o: ../Source/bucketalloc.c \
- ../Source/../Include/tesselator.h
-
-../Source/../Include/tesselator.h:
--- ext/oF_deps/master/custom/tess2/repo/Build/obj/Debug/tess2/geom.d
+++
@@ -1,8 +0,0 @@
-obj/Debug/tess2/geom.o: ../Source/geom.c ../Source/mesh.h \
- ../Source/../Include/tesselator.h ../Source/geom.h
-
-../Source/mesh.h:
-
-../Source/../Include/tesselator.h:
-
-../Source/geom.h:
