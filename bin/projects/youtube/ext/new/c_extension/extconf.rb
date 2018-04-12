12d2ffabdbe31253fd5e290c2fbd72acb1a7ee6f
Raven <RavensKrag@gmail.com>
2018-04-12 01:07:05 -0400

    build system is working, but program doesn't work because something in the 'youtube' project is busted.

--- bin/projects/youtube/ext/new/c_extension/extconf.rb
+++ bin/projects/youtube/ext/new/c_extension/extconf.rb
@@ -198,8 +198,8 @@ of_core_libs_dynamic_flags =
 
 
 list_of_linker_flags = [
-	# of_project_objs,
-	# of_project_addon_objs,
+	of_project_objs,
+	of_project_addon_objs,
 	# ^ these two will now be part of a static library (.a file)
 	# OF_SKETCH_LIB_FILE, # <-- here is that library
 	
