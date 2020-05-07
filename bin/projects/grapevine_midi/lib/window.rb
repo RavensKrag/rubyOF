981ebfe87015396d3a5ecb073ed46739fbc4a4c3
Raven <RavensKrag@gmail.com>
2020-05-07 09:14:50 -0400

    Move input handler code to separate file.

--- bin/projects/grapevine_midi/lib/window.rb
+++ bin/projects/grapevine_midi/lib/window.rb
@@ -20,132 +20,10 @@ LIB_DIR = current_file.parent
 require LIB_DIR/'helpers.rb'
 # require_all LIB_DIR/'history'
 
+require LIB_DIR/'input_handler.rb'
+
+
 require LIB_DIR/'ofx_extensions.rb'
-
-
-
-# input handler will supress OS-level key repeat events
-class InputHandler
-  def initialize
-    @buttons = Hash.new
-    @callbacks = Hash.new
-  end
-  
-  def register_callback(btn, &block)
-    @buttons[btn] = false
-    
-    helper = InputHandlerHelper.new
-    block.call(helper)
-    @callbacks[btn] = helper
-  end
-  
-  # 4 states
-  # ----
-  # idle
-  # positive edge
-  # active
-  # negative edge
-  
-  def update
-    @callbacks.each do |btn, callback_obj|
-      current_state = @buttons[btn]
-      
-      # p callback_obj
-      
-      # require 'irb'
-      # binding.irb
-      
-      if current_state
-        # hi = active
-        callback_obj.while_active_call()
-      else
-        # low = idle
-        callback_obj.while_idle_call()
-      end
-    end
-  end
-  
-  def key_pressed(id)
-    if id == OF_KEY_ESC
-      return
-      # bypass this whole thing when the escape key is pressed
-      # (escape quits the app)
-    end
-    if @buttons[id].nil?
-      warn "ERROR: no callbacks registered for button id: #{id}"
-      return
-    end
-    
-    current_state = @buttons[id]
-    
-    if current_state
-      # hi -> hi
-      # NO-OP
-    else
-      # low -> hi
-      # positive edge
-      callback_obj = @callbacks[id]
-      callback_obj.on_press_call()
-    end
-        
-    
-    @buttons[id] = true
-  end
-  
-  def key_released(id)
-    if id == OF_KEY_ESC
-      return
-      # bypass this whole thing when the escape key is pressed
-      # (escape quits the app)
-    end
-    if @buttons[id].nil?
-      warn "ERROR: no callbacks registered for button id: #{id}"
-      return
-    end
-    
-    
-    current_state = @buttons[id]
-    
-    if current_state
-      # hi -> low
-      callback_obj = @callbacks[id]
-      callback_obj.on_release_call()
-    else
-      # low -> low
-      # NO-OP
-    end
-    
-    @buttons[id] = false
-  end
-  
-  private
-  
-  
-  class InputHandlerHelper
-    def on_press(&block)
-      @on_press = block
-    end
-    
-    def on_release(&block)
-      @on_release = block
-    end
-    
-    def while_idle(&block)
-      puts "idle callback set"
-      @while_idle = block
-    end
-    
-    def while_active(&block)
-      @while_active = block
-    end
-    
-    [:on_press, :on_release, :while_idle, :while_active].each do |sym|
-      define_method "#{sym}_call" do |*args|
-        instance_variable_get("@#{sym}").call(*args)
-      end
-    end
-  end
-end
 
 
 class Window < RubyOF::Window
@@ -205,6 +83,39 @@ class Window < RubyOF::Window
         
       end
     end
+    
+    
+    btn_id = 117 # the 'u' key
+    @input_handler.register_callback(btn_id) do |btn|
+      btn.on_press do
+        puts "press x"
+        
+        channel = 3
+        note = 72
+        velocity = 64
+        @cpp_ptr["midiOut"].sendNoteOn(channel, note, velocity)
+      end
+      
+      btn.on_release do
+        puts "release x"
+        
+        channel = 3
+        note = 72
+        velocity = 64
+        @cpp_ptr["midiOut"].sendNoteOff(channel, note, velocity)
+      end
+      
+      btn.while_idle do
+        
+      end
+      
+      btn.while_active do
+        
+      end
+    end
+    
+    # if you try to send two notes at once, then synth freaks out and gets stuck - not sure why, but I need to fix that asap to have proper communication via midi
+    
     
   end
   
