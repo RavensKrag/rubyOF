3314d14107bc9c5e936807b21c4e2319e6ce54de
Raven <RavensKrag@gmail.com>
2020-05-19 08:34:41 -0400

    Remove C++ level midi data printing

--- bin/projects/grapevine_midi/ext/c_extension/app.cpp
+++ bin/projects/grapevine_midi/ext/c_extension/app.cpp
@@ -368,54 +368,6 @@ void rbApp::draw(){
 	
 	
 	
-	for(unsigned int i = 0; i < midiMessages.size(); ++i) {
-		
-		ofxMidiMessage &message = midiMessages[i];
-		int x = 370;
-		int y = i*40 + 40;
-		
-		// draw the last recieved message contents to the screen,
-		// this doesn't print all the data from every status type
-		// but you should get the general idea
-		stringstream text;
-		text << ofxMidiMessage::getStatusString(message.status);
-		while(text.str().length() < 16) { // pad status width
-			text << " ";
-		}
-		
-		ofSetColor(127);
-		if(message.status < MIDI_SYSEX) {
-			text << "chan: " << message.channel;
-			if(message.status == MIDI_CONTROL_CHANGE) {
-				text << "\tctl: " << message.control;
-				ofDrawRectangle(x + ofGetWidth()*0.2, y + 12,
-					ofMap(message.control, 0, 127, 0, ofGetWidth()*0.2), 10);
-			}
-			else if(message.status == MIDI_PITCH_BEND) {
-				text << "\tval: " << message.value;
-				ofDrawRectangle(x + ofGetWidth()*0.2, y + 12,
-					ofMap(message.value, 0, MIDI_MAX_BEND, 0, ofGetWidth()*0.2), 10);
-			}
-			else {
-				text << "\tpitch: " << message.pitch;
-				ofDrawRectangle(x + ofGetWidth()*0.2, y + 12,
-					ofMap(message.pitch, 0, 127, 0, ofGetWidth()*0.2), 10);
-
-				text << "\tvel: " << message.velocity;
-				ofDrawRectangle(x + (ofGetWidth()*0.2 * 2), y + 12,
-					ofMap(message.velocity, 0, 127, 0, ofGetWidth()*0.2), 10);
-			}
-			text << " "; // pad for delta print
-		}
-
-		text << "delta: " << message.deltatime;
-		ofSetColor(0);
-		ofDrawBitmapString(text.str(), x, y);
-		text.str(""); // clear
-	}
-	
-	
-	
 	
 	
 	// ========================================
