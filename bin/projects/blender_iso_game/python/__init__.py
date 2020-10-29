0a7fc86e8163dc6df9fdd0b8b54aa4f285dd3a8d
Raven <RavensKrag@gmail.com>
2020-10-29 18:06:53 -0400

    Commit blender python addon script
    Not sure if this draft will install as a full addon,
    but for now I can edit it externally and open in blender to run.

---
+++ bin/projects/blender_iso_game/python/__init__.py
@@ -0,0 +1,491 @@
+bl_info = {
+    "name": "RubyOF renderer engine",
+    "author": "Jason Ko",
+    "version": (0, 0, 1),
+    "blender": (2, 90, 1),
+    "location": "Render",
+    "description": "Integration with external real-time RubyOF renderer for games, etc",
+    "category": "Render",
+}
+
+import json
+
+import os
+import fcntl
+import posix
+
+import bpy
+import bgl
+
+import bpy_extras
+import bpy_extras.view3d_utils
+
+from bpy.props import (StringProperty,
+                       BoolProperty,
+                       IntProperty,
+                       FloatProperty,
+                       EnumProperty,
+                       )
+
+class RubyOF(bpy.types.RenderEngine):
+    # These three members are used by blender to set up the
+    # RenderEngine; define its internal name, visible name and capabilities.
+    bl_idname = "RUBYOF"
+    bl_label = "RubyOF"
+    bl_use_preview = True
+
+    # Init is called whenever a new render engine instance is created. Multiple
+    # instances may exist at the same time, for example for a viewport and final
+    # render.
+    def __init__(self):        
+        self.scene_data = None
+        self.draw_data = None
+        self.fifo_path = "/home/ravenskrag/Desktop/gem_structure/bin/projects/blender_iso_game/bin/run/blender_comm"
+        
+        self.fifo = open(self.fifo_path, 'w')
+        print("FIFO open")
+
+    # When the render engine instance is destroy, this is called. Clean up any
+    # render engine data here, for example stopping running render threads.
+    def __del__(self):
+        self.fifo.close()
+        print("FIFO closed")
+
+    # This is the method called by Blender for both final renders (F12) and
+    # small preview for materials, world and lights.
+    def render(self, depsgraph):
+        scene = depsgraph.scene
+        scale = scene.render.resolution_percentage / 100.0
+        self.size_x = int(scene.render.resolution_x * scale)
+        self.size_y = int(scene.render.resolution_y * scale)
+
+        # Fill the render result with a flat color. The framebuffer is
+        # defined as a list of pixels, each pixel itself being a list of
+        # R,G,B,A values.
+        if self.is_preview:
+            color = [0.1, 0.2, 0.1, 1.0]
+        else:
+            color = [0.2, 0.1, 0.1, 1.0]
+
+        pixel_count = self.size_x * self.size_y
+        rect = [color] * pixel_count
+
+        # Here we write the pixel values to the RenderResult
+        result = self.begin_result(0, 0, self.size_x, self.size_y)
+        layer = result.layers[0].passes["Combined"]
+        layer.rect = rect
+        self.end_result(result)
+
+    # For viewport renders, this method gets called once at the start and
+    # whenever the scene or 3D viewport changes. This method is where data
+    # should be read from Blender in the same thread. Typically a render
+    # thread will be started to do the work while keeping Blender responsive.
+    def view_update(self, context, depsgraph):
+        region = context.region
+        view3d = context.space_data
+        scene = depsgraph.scene
+
+        # Get viewport dimensions
+        dimensions = region.width, region.height
+        print("view update ---")
+        if not self.scene_data:
+            # First time initialization
+            self.scene_data = []
+            first_time = True
+
+            # Loop over all datablocks used in the scene.
+            for datablock in depsgraph.ids:
+                pass
+        else:
+            first_time = False
+
+            # Test which datablocks changed
+            for update in depsgraph.updates:
+                print("Datablock updated: ", update.id.name)
+
+            # Test if any material was added, removed or changed.
+            if depsgraph.id_type_updated('MATERIAL'):
+                print("Materials updated")
+        
+        
+        
+        
+        print("FIFO open")
+#        text = text.encode('utf-8')
+        pipe = open(self.fifo_path, 'w')
+        
+        
+        
+        
+        # Loop over all object instances in the scene.
+        if first_time or depsgraph.id_type_updated('OBJECT'):
+            print("obj update detected")
+            for instance in depsgraph.object_instances:
+                obj = instance.object
+#                print(instance)
+#                print(obj.type)
+                if obj.type == 'MESH':
+                    print(obj)
+                    
+                    rot = obj.rotation_quaternion
+                    pos = obj.location
+                    
+                    data = [
+                        {
+                            'name': obj.name_full,
+                            'rotation':[
+                                "Quat",
+                                rot.w,
+                                rot.x,
+                                rot.y,
+                                rot.z
+                            ],
+                            'position':[
+                                "Vec3",
+                                pos.x,
+                                pos.y,
+                                pos.z
+                            ]
+                        }
+                    ]
+                    pipe.write(json.dumps(data) + "\n")
+                    pipe.close()
+        
+        print("---")
+        
+        
+
+    # For viewport renders, this method is called whenever Blender redraws
+    # the 3D viewport. The renderer is expected to quickly draw the render
+    # with OpenGL, and not perform other expensive work.
+    # Blender will draw overlays for selection and editing on top of the
+    # rendered image automatically.
+    def view_draw(self, context, depsgraph):
+        #
+        # Update the viewport camera
+        #
+        region = context.region
+        rv3d = context.region_data # RegionView3D(bpy_struct)
+        coord = (region.width/2.0, region.height/2.0)
+        
+        camera_direction = bpy_extras.view3d_utils.region_2d_to_vector_3d(region, rv3d, coord)
+        camera_origin    = bpy_extras.view3d_utils.region_2d_to_origin_3d(region, rv3d, coord)
+        
+        space = context.space_data # SpaceView3D(Space)
+        
+#        print(rv3d.view_rotation)
+        print(camera_direction)
+#        print(camera_origin)
+#        print(context.space_data.lens)
+        # ^ note: camera objects have both lens (mm) and angle (fov degrees)
+        
+        print(space.clip_start)
+        print(space.clip_end)
+        # ^ these are viewport properties, not camera object properties
+        
+        
+        print("FIFO open")
+#        text = text.encode('utf-8')
+        pipe = open(self.fifo_path, 'w')
+        
+#        pipe.write("hello world\n")
+
+        
+        data = [
+            {
+                'name': 'viewport_camera',
+                'rotation':[
+                    "Quat",
+                    rv3d.view_rotation.w,
+                    rv3d.view_rotation.x,
+                    rv3d.view_rotation.y,
+                    rv3d.view_rotation.z
+                ],
+                'position':[
+                    "Vec3",
+                    camera_origin.x,
+                    camera_origin.y,
+                    camera_origin.z
+                ],
+                'lens':[
+                    "mm",
+                    context.space_data.lens
+                ]
+            }
+        ]
+        pipe.write(json.dumps(data) + "\n")
+        pipe.close()
+        
+#        io = posix.open(fifo_path, posix.O_RDWR)
+#        print(io)
+#        
+        
+        
+#        posix.write("hello world\n")
+#        posix.close(io)
+        
+#        io = os.open(fifo_path, os.O_RDONLY | os.O_NONBLOCK)
+#        io.close()
+#        fifo = open(fifo_path, 'w')
+        
+#        flag = fcntl.fcntl(fifo, fcntl.F_GETFL)
+#        fcntl.fcntl(fifo, fcntl.F_SETFL, flag | os.O_NONBLOCK)
+        
+#        fifo.write("hello world")
+#        fifo.close()
+#        print("FIFO closed")
+        
+        
+#        with open(fifo_path) as fifo:
+#            print("fifo opened")
+#            while True:
+#                data = fifo.read()
+#                if len(data) == 0:
+#                    print("writer closed")
+#                    break
+                
+        
+        
+        
+        
+        #
+        # Render the viewport
+        #
+        region = context.region
+        scene = depsgraph.scene
+
+        # Get viewport dimensions
+        dimensions = region.width, region.height
+
+        # Bind shader that converts from scene linear to display space,
+        bgl.glEnable(bgl.GL_BLEND)
+        bgl.glBlendFunc(bgl.GL_ONE, bgl.GL_ONE_MINUS_SRC_ALPHA)
+        self.bind_display_space_shader(scene)
+
+        if not self.draw_data or self.draw_data.dimensions != dimensions:
+            self.draw_data = CustomDrawData(dimensions, context)
+
+        self.draw_data.draw()
+
+        self.unbind_display_space_shader()
+        bgl.glDisable(bgl.GL_BLEND)
+
+
+class CustomDrawData:
+    def __init__(self, dimensions, context):
+        self.context = context
+        
+        # Generate dummy float image buffer
+        self.dimensions = dimensions
+        width, height = dimensions
+
+        pixels = [0.1, 0.2, 0.1, 1.0] * width * height
+        pixels = bgl.Buffer(bgl.GL_FLOAT, width * height * 4, pixels)
+
+        # Generate texture
+        self.texture = bgl.Buffer(bgl.GL_INT, 1)
+        bgl.glGenTextures(1, self.texture)
+        bgl.glActiveTexture(bgl.GL_TEXTURE0)
+        bgl.glBindTexture(bgl.GL_TEXTURE_2D, self.texture[0])
+        bgl.glTexImage2D(bgl.GL_TEXTURE_2D, 0, bgl.GL_RGBA16F, width, height, 0, bgl.GL_RGBA, bgl.GL_FLOAT, pixels)
+        bgl.glTexParameteri(bgl.GL_TEXTURE_2D, bgl.GL_TEXTURE_MIN_FILTER, bgl.GL_LINEAR)
+        bgl.glTexParameteri(bgl.GL_TEXTURE_2D, bgl.GL_TEXTURE_MAG_FILTER, bgl.GL_LINEAR)
+        bgl.glBindTexture(bgl.GL_TEXTURE_2D, 0)
+
+        # Bind shader that converts from scene linear to display space,
+        # use the scene's color management settings.
+        shader_program = bgl.Buffer(bgl.GL_INT, 1)
+        bgl.glGetIntegerv(bgl.GL_CURRENT_PROGRAM, shader_program)
+
+        # Generate vertex array
+        self.vertex_array = bgl.Buffer(bgl.GL_INT, 1)
+        bgl.glGenVertexArrays(1, self.vertex_array)
+        bgl.glBindVertexArray(self.vertex_array[0])
+
+        texturecoord_location = bgl.glGetAttribLocation(shader_program[0], "texCoord")
+        position_location = bgl.glGetAttribLocation(shader_program[0], "pos")
+
+        bgl.glEnableVertexAttribArray(texturecoord_location)
+        bgl.glEnableVertexAttribArray(position_location)
+
+        # Generate geometry buffers for drawing textured quad
+        position = [0.0, 0.0, width, 0.0, width, height, 0.0, height]
+        position = bgl.Buffer(bgl.GL_FLOAT, len(position), position)
+        texcoord = [0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0]
+        texcoord = bgl.Buffer(bgl.GL_FLOAT, len(texcoord), texcoord)
+
+        self.vertex_buffer = bgl.Buffer(bgl.GL_INT, 2)
+
+        bgl.glGenBuffers(2, self.vertex_buffer)
+        bgl.glBindBuffer(bgl.GL_ARRAY_BUFFER, self.vertex_buffer[0])
+        bgl.glBufferData(bgl.GL_ARRAY_BUFFER, 32, position, bgl.GL_STATIC_DRAW)
+        bgl.glVertexAttribPointer(position_location, 2, bgl.GL_FLOAT, bgl.GL_FALSE, 0, None)
+
+        bgl.glBindBuffer(bgl.GL_ARRAY_BUFFER, self.vertex_buffer[1])
+        bgl.glBufferData(bgl.GL_ARRAY_BUFFER, 32, texcoord, bgl.GL_STATIC_DRAW)
+        bgl.glVertexAttribPointer(texturecoord_location, 2, bgl.GL_FLOAT, bgl.GL_FALSE, 0, None)
+
+        bgl.glBindBuffer(bgl.GL_ARRAY_BUFFER, 0)
+        bgl.glBindVertexArray(0)
+
+    def __del__(self):
+        bgl.glDeleteBuffers(2, self.vertex_buffer)
+        bgl.glDeleteVertexArrays(1, self.vertex_array)
+        bgl.glBindTexture(bgl.GL_TEXTURE_2D, 0)
+        bgl.glDeleteTextures(1, self.texture)
+
+    def draw(self):
+        a = self.context.scene.my_custom_props.alpha
+        bgl.glClearColor(0*a,0*a,0*a,a)
+        bgl.glClear(bgl.GL_COLOR_BUFFER_BIT|bgl.GL_DEPTH_BUFFER_BIT)
+#        bgl.glActiveTexture(bgl.GL_TEXTURE0)
+#        bgl.glBindTexture(bgl.GL_TEXTURE_2D, self.texture[0])
+#        bgl.glBindVertexArray(self.vertex_array[0])
+#        bgl.glDrawArrays(bgl.GL_TRIANGLE_FAN, 0, 4)
+#        bgl.glBindVertexArray(0)
+#        bgl.glBindTexture(bgl.GL_TEXTURE_2D, 0)
+
+
+
+
+
+
+#
+# Properties
+#
+
+class RubyOF_Properties(bpy.types.PropertyGroup):
+    my_bool: BoolProperty(
+        name="Enable or Disable",
+        description="A bool property",
+        default = False
+        )
+        
+    my_float: FloatProperty(
+        name = "Float Value",
+        description = "A float property",
+        default = 23.7,
+        min = 0.01,
+        max = 30.0
+        )
+        
+    my_pointer: bpy.props.PointerProperty(type=bpy.types.Object)
+    
+    alpha: FloatProperty(
+        name = "Alpha",
+        description = "Alpha transparency for the window",
+        default = 0.25,
+        min = 0.0,
+        max = 1.0
+        )
+        
+    b_windowLink: BoolProperty(
+        name="window link",
+        description="Automatically reposition and resize the RubyOF window to be directly under the Blender 3D view",
+        default = False
+        )
+    
+    camera: bpy.props.PointerProperty(
+        type=bpy.types.Camera,
+        name="camera",
+        description="Camera to be used by the RubyOF game engine")
+
+
+#
+# Panel for properties (under Render Properties tab)
+#
+class RubyOF_PropertiesPanel(bpy.types.Panel):
+    COMPAT_ENGINES= {"RUBYOF"}
+    
+    bl_idname = "RUBYOF_PT_HELLOWORLD"
+    bl_label = "Properties (custom renderer)"
+    bl_region_type = 'WINDOW'
+    bl_context = "render"
+    bl_space_type = 'PROPERTIES'
+    
+    # poll allows panel to only be shown in certain contexts (ie, when the function returns true)
+    # Here, we check to make sure that the active render engine is in the list of compatible engines.
+    @classmethod
+    def poll(cls, context):
+        print(context.engine)
+        return (context.engine in cls.COMPAT_ENGINES)
+    
+    def draw(self, context):
+        # self.layout.label(text="Hello World")
+        
+        # self.layout.prop(context.scene.my_custom_props, "my_bool")
+        # self.layout.prop(context.scene.my_custom_props, "my_float")
+        # self.layout.prop(context.scene.my_custom_props, "my_pointer")
+        
+        # self.layout.label(text="Real Data Below")
+        self.layout.prop(context.scene.my_custom_props, "alpha")
+        self.layout.prop(context.scene.my_custom_props, "b_windowLink")
+        self.layout.prop(context.scene.my_custom_props, "camera")
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+# RenderEngines also need to tell UI Panels that they are compatible with.
+# We recommend to enable all panels marked as BLENDER_RENDER, and then
+# exclude any panels that are replaced by custom panels registered by the
+# render engine, or that are not supported.
+def get_panels():
+    exclude_panels = {
+        'VIEWLAYER_PT_filter',
+        'VIEWLAYER_PT_layer_passes',
+    }
+    
+    panels = []
+    for panel in bpy.types.Panel.__subclasses__():
+        if hasattr(panel, 'COMPAT_ENGINES') and 'BLENDER_RENDER' in panel.COMPAT_ENGINES:
+            if panel.__name__ not in exclude_panels:
+                panels.append(panel)
+    
+    return panels
+
+classes = (
+    RubyOF_Properties,
+    RubyOF_PropertiesPanel
+)
+
+def register():
+    # Register the RenderEngine
+    bpy.utils.register_class(RubyOF)
+    
+    for panel in get_panels():
+        panel.COMPAT_ENGINES.add('RUBYOF')
+    
+    
+    
+    for c in classes:
+        bpy.utils.register_class(c)
+    
+    # Bind variable for properties
+    bpy.types.Scene.my_custom_props = bpy.props.PointerProperty(type=RubyOF_Properties)
+    
+
+
+def unregister():
+    bpy.utils.unregister_class(RubyOF)
+    
+    for panel in get_panels():
+        if 'RUBYOF' in panel.COMPAT_ENGINES:
+            panel.COMPAT_ENGINES.remove('RUBYOF')
+    
+    for c in classes:
+        bpy.utils.unregister_class(c)
+    
+
+
+if __name__ == "__main__":
+    print("hello world")
+    register()
