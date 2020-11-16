a15ea837e2c499ebf8916a23ff2cd085f809f5e9
Raven <RavensKrag@gmail.com>
2020-11-16 18:55:37 -0500

    Save 3D world to file - serialize light properties as well

--- bin/projects/blender_iso_game/lib/core.rb
+++ bin/projects/blender_iso_game/lib/core.rb
@@ -39,6 +39,7 @@ end
 
 
 class BlenderObject
+  attr_accessor :name
   attr_accessor :dirty
   
   def initialize
@@ -53,7 +54,6 @@ class BlenderCube < BlenderObject
   
   attr_reader :mesh, :node
   attr_accessor :color
-  attr_accessor :name
   
   def initialize
     @mesh = RubyOF::Mesh.new
@@ -393,7 +393,95 @@ class CustomCamera< BlenderObject
 end
 
 class BlenderLight < BlenderObject
+  extend Forwardable
   
+  def initialize
+    @light = RubyOF::Light.new
+    
+    setPointLight()
+    
+    @size = nil
+    @size_x = nil
+    @size_y = nil
+  end
+  
+  
+  def setPointLight()
+    @type = 'POINT'
+    
+    @light.setPointLight()
+  end
+  
+  def setDirectional()
+    @type = 'SUN'
+    
+    @light.setDirectional()
+  end
+  
+  def setSpotlight(cutoff_radians, exponent)
+    @type = 'SPOT'
+    
+    @size = cutoff_radians
+    
+    size_deg = cutoff_radians / (2*Math::PI) * 360
+    @light.setSpotlight(size_deg, 0) # requires 2 args
+    # float spotCutOff=45.f, float exponent=0.f
+  end
+  
+  def setAreaLight(width, height)
+    @type = 'AREA'
+    
+    @size_x = width
+    @size_y = height
+    
+    light.setAreaLight(@size_x, @size_y)
+  end
+  
+  
+  def_delegators :@light, :position, :orientation, :scale,
+                          :position=, :orientation=, :scale=,
+                          :enable, :disable, :enabled?,
+                          :diffuse_color=, :specular_color=, :ambient_color=,
+                          :diffuse_color
+  
+  
+  def data_dump
+    orientation = self.orientation
+    position = self.position
+    scale = self.scale
+    
+    color = self.diffuse_color.to_a
+            .first(3) # discard alpha component
+            .map{|x| x / 255.0 } # convert to float from 0..1
+    
+    {
+        'type' => 'LIGHT',
+        'name' =>  @name,
+        'light_type' => @type,
+        'rotation' => [
+          'Quat',
+          orientation.w, orientation.x, orientation.y, orientation.z
+        ],
+        'position' => [
+          'Vec3',
+          position.x, position.y, position.z
+        ],
+        'scale' => [
+          'Vec3',
+          scale.x, scale.y, scale.z
+        ],
+        'color' => ['rgb'] + color,
+        'size' => [
+          'radians', @size
+        ],
+        'size_x' => [
+          'float', @size_x
+        ],
+        'size_y' => [
+          'float', @size_y
+        ]
+    }
+  end
 end
 
 
@@ -578,6 +666,8 @@ class BlenderSync
           scale = GLM::Vec3.new(*(obj['scale'][1..3]))
           
           light = @entities['Light']
+          
+          light.name = obj['name']
           
           light.disable()
           
@@ -826,7 +916,7 @@ class Core
       'viewport_camera' => CustomCamera.new,
       
       'Cube'  => BlenderCube.new,
-      'Light' => RubyOF::Light.new
+      'Light' => BlenderLight.new
     }
     
     
@@ -884,6 +974,7 @@ class Core
       entity_data_list = [
         @entities['viewport_camera'].data_dump,
         @entities['Cube'].data_dump,
+        @entities['Light'].data_dump
       ]
       
       # obj['view_perspective'] # [PERSP', 'ORTHO', 'CAMERA']
@@ -1103,12 +1194,6 @@ class Core
         
         
         
-        
-        light_color = @entities['Light'].diffuse_color
-        
-        
-        
-        
         @mat1 ||= RubyOF::Material.new
         # @mat1.diffuse_color = RubyOF::Color.rgb([0, 255, 0])
         @mat1.diffuse_color = RubyOF::Color.rgb([255, 255, 255])
@@ -1118,22 +1203,20 @@ class Core
         
         
         @mat2 ||= RubyOF::Material.new
-        @mat2.emissive_color = light_color
-        
-        
+        # ^ update color of this material every time the light color changes
+        #   not just on the first frame
+        #   (creating the material is the expensive part anyway)
         
         
         @first_draw = false
       end
       
+      light_color = @entities['Light'].diffuse_color
+      @mat2.emissive_color = light_color
+      
       
       # light_pos = GLM::Vec3.new(4,-5,3);
       # cube_pos = GLM::Vec3.new(0,0,0);
-      
-      # TODO: position of cube and light need to be saved to file (like camera values) or on the first frame the system doesn't know where to put them
-        # (defaults to origin, 0,0,0, which is awkward because
-        # then the light is inside the cube)
-        # If you click on any object in blender, the values reset, but it's annoying / confusing to have to do that every time you start the app up.
       
       
       ofEnableDepthTest()
