ec2b50d4c276dc02458d49f8f7d2b233855ec2ed
Raven <RavensKrag@gmail.com>
2020-11-29 00:10:26 -0500

    Ruby-level color interface

--- lib/rubyOF.rb
+++ lib/rubyOF.rb
@@ -170,12 +170,14 @@ end
 
 
 class Color
+	CHANNEL_MAX = 255
+	
 	def to_a
 		return [self.r,self.g,self.b,self.a]
 	end
 	
 	def rgba=(color_array)
-		raise ArgumentError, "Expected an array of size 4 that encodes rgba color data (each channel should be an integer, with a maximum of 255 per channel)" unless color_array.size == 4
+		raise ArgumentError, "Expected an array of size 4 that encodes rgba color data (each channel should be an integer, with a maximum of #{CHANNEL_MAX} per channel)" unless color_array.size == 4
 		
 		self.r,self.g,self.b,self.a = color_array
 	end
@@ -189,17 +191,17 @@ class Color
 		end
 		
 		def rgb(color_array)
-			raise ArgumentError, "Expected an array of size 3 that encodes rgb color data (each channel should be an integer, with a maximum of 255 per channel)" unless color_array.size == 3
+			raise ArgumentError, "Expected an array of size 3 that encodes rgb color data (each channel should be an integer, with a maximum of #{CHANNEL_MAX} per channel)" unless color_array.size == 3
 			
 			# Must add, can't push because arrays in Ruby are objects
 			# and all objects in Ruby are reference types.
 			# Thus, the array provided will always be an in/out parameter.
-			self.rgba(color_array + [255])
+			self.rgba(color_array + [CHANNEL_MAX])
 		end
 		
 		def hex(hex)
 			color = self.new
-			color.set_hex(hex, 255)
+			color.set_hex(hex, CHANNEL_MAX)
 			return color
 		end
 		
@@ -210,6 +212,95 @@ class Color
 		end
 	end
 end
+
+class FloatColor
+	CHANNEL_MAX = 1
+	
+	def to_a
+		return [self.r,self.g,self.b,self.a]
+	end
+	
+	def rgba=(color_array)
+		raise ArgumentError, "Expected an array of size 4 that encodes rgba color data (each channel should be an float, in the range 0 to 1)" unless color_array.size == 4
+		
+		self.r,self.g,self.b,self.a = color_array
+	end
+	
+	class << self
+		def rgba(color_array)
+			color = self.new
+			color.rgba = color_array
+			
+			return color
+		end
+		
+		def rgb(color_array)
+			raise ArgumentError, "Expected an array of size 3 that encodes rgb color data (each channel should be an float, in the range 0 to 1)" unless color_array.size == 3
+			
+			# Must add, can't push because arrays in Ruby are objects
+			# and all objects in Ruby are reference types.
+			# Thus, the array provided will always be an in/out parameter.
+			self.rgba(color_array + [CHANNEL_MAX])
+		end
+		
+		def hex(hex)
+			color = self.new
+			color.set_hex(hex, CHANNEL_MAX)
+			return color
+		end
+		
+		def hex_alpha(hex, alpha)
+			color = self.new
+			color.set_hex(hex, alpha)
+			return color
+		end
+	end
+end
+
+class ShortColor
+	CHANNEL_MAX = 65535
+	
+	def to_a
+		return [self.r,self.g,self.b,self.a]
+	end
+	
+	def rgba=(color_array)
+		raise ArgumentError, "Expected an array of size 4 that encodes rgba color data (each channel should be an float, in the range 0 to 1)" unless color_array.size == 4
+		
+		self.r,self.g,self.b,self.a = color_array
+	end
+	
+	class << self
+		def rgba(color_array)
+			color = self.new
+			color.rgba = color_array
+			
+			return color
+		end
+		
+		def rgb(color_array)
+			raise ArgumentError, "Expected an array of size 3 that encodes rgb color data (each channel should be an float, in the range 0 to 1)" unless color_array.size == 3
+			
+			# Must add, can't push because arrays in Ruby are objects
+			# and all objects in Ruby are reference types.
+			# Thus, the array provided will always be an in/out parameter.
+			self.rgba(color_array + [CHANNEL_MAX])
+		end
+		
+		def hex(hex)
+			color = self.new
+			color.set_hex(hex, CHANNEL_MAX)
+			return color
+		end
+		
+		def hex_alpha(hex, alpha)
+			color = self.new
+			color.set_hex(hex, alpha)
+			return color
+		end
+	end
+end
+
 	
 class Shader
 	# private :load_oneNameVertAndFrag, :load_VertFragGeom
