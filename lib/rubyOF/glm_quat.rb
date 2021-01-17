9f0ee2545abffedece274bef9076a57a2dc79f71
Raven <RavensKrag@gmail.com>
2021-01-16 22:44:40 -0500

    serialize GLM vector types and quaternion with YAML

--- lib/rubyOF/glm_quat.rb
+++ lib/rubyOF/glm_quat.rb
@@ -1,80 +1,105 @@
 module GLM
 
 class Quat
-	include RubyOF::Freezable
-	
-	def to_s
-		format = '%.03f'
-		w = format % self.w
-		x = format % self.x
-		y = format % self.y
-		z = format % self.z
-		
-		return "(#{w}, #{x}, #{y}, #{z})"
-	end
-	
-	def inspect
-		super()
-	end
-	
-	
-	
-	# hide C++ level helper methods
-	private :get_component
-	private :set_component
-	
-	
-	# 
-	# glm::quat internal memory order is (x,y,z,w)
-	# but the constructor specifies (w,x,y,z)
-	# and glm::string_cast uses (w,x,y,z)
-	# Therefore, direct memory access should not be allowed from Ruby
-	# and all interactions with quaternions should be done through the
-	# named component interface
-	# 
-	# src: https://stackoverflow.com/questions/48348509/glmquat-why-the-order-of-x-y-z-w-components-are-mixed
-	
-	
-	# 
-	# no numerical index interface for Quaternion,
-	# unlike the other vector types
-	# see above for explanation.
-	# 
-	
-	# # get / set value of a component by numerical index
-	# def [](i)
-	# 	return get_component(i)
-	# end
-	
-	# def []=(i, value)
-	# 	return set_component(i, value.to_f)
-	# end
-	
-	
-	# get / set values of component by axis name
-	%w[x y z w].each_with_index do |component, i|
-		# getters
-		# (same as array-style interface)
-		define_method component do
-			get_component(i)
-		end 
-		
-		# setters
-		# (use special C++ function to make sure data is written back to C++ land)
-		define_method "#{component}=" do |value|
-			set_component(i, value.to_f)
-		end 
-	end
-	
-	
-	# 
-	# can automatically convert vec3 and vec2 to CP::Vec2,
-	# but no built-in conversion for vec4
-	# 
-	
-	# def to_cpvec2
-	# 	return CP::Vec2.new(self.x, self.y)
-	# end
+  include RubyOF::Freezable
+  
+  
+  def to_a
+    return [w,x,y,z]
+  end
+  
+  def to_s
+    format = '%.03f'
+    w = format % self.w
+    x = format % self.x
+    y = format % self.y
+    z = format % self.z
+    
+    return "(#{w}, #{x}, #{y}, #{z})"
+  end
+  
+  
+  def inspect
+    super()
+  end
+  
+  
+  
+  # hide C++ level helper methods
+  private :get_component
+  private :set_component
+  
+  
+  # 
+  # glm::quat internal memory order is (x,y,z,w)
+  # but the constructor specifies (w,x,y,z)
+  # and glm::string_cast uses (w,x,y,z)
+  # Therefore, direct memory access should not be allowed from Ruby
+  # and all interactions with quaternions should be done through the
+  # named component interface
+  # 
+  # src: https://stackoverflow.com/questions/48348509/glmquat-why-the-order-of-x-y-z-w-components-are-mixed
+  
+  
+  # 
+  # no numerical index interface for Quaternion,
+  # unlike the other vector types
+  # see above for explanation.
+  # 
+  
+  # # get / set value of a component by numerical index
+  # def [](i)
+  #   return get_component(i)
+  # end
+  
+  # def []=(i, value)
+  #   return set_component(i, value.to_f)
+  # end
+  
+  
+  # get / set values of component by axis name
+  %w[x y z w].each_with_index do |component, i|
+    # getters
+    # (same as array-style interface)
+    define_method component do
+      get_component(i)
+    end 
+    
+    # setters
+    # (use special C++ function to make sure data is written back to C++ land)
+    define_method "#{component}=" do |value|
+      set_component(i, value.to_f)
+    end 
+  end
+  
+  
+  # 
+  # can automatically convert vec3 and vec2 to CP::Vec2,
+  # but no built-in conversion for vec4
+  # 
+  
+  # def to_cpvec2
+  #   return CP::Vec2.new(self.x, self.y)
+  # end
+  
+  # 
+  # YAML serialization interface
+  # 
+  
+  def to_yaml_type
+    "!ruby/object:#{self.class}"
+  end
+  
+  def encode_with(coder)
+    coder.represent_seq to_yaml_type, self.to_a
+  end
+  
+  def init_with(coder)
+    w,x,y,z = coder.seq
+    
+    initialize(w,x,y,z)
+  end
 end
+
 
 end
--- lib/rubyOF/glm_vec3.rb
+++ lib/rubyOF/glm_vec3.rb
@@ -1,62 +1,84 @@
 module GLM
 
 class Vec3
-	include RubyOF::Freezable
-	
-	def to_a
-		return [self.x, self.y, self.z]
-	end
-	
-	def to_s
-		format = '%.03f'
-		x = format % self.x
-		y = format % self.y
-		z = format % self.z
-		
-		return "(#{x}, #{y}, #{z})"
-	end
-	
-	def inspect
-		super()
-	end
-	
-	
-	
-	# hide C++ level helper methods
-	private :get_component
-	private :set_component
-	
-	
-	# get / set value of a component by numerical index
-	def [](i)
-		return get_component(i)
-	end
-	
-	def []=(i, value)
-		return set_component(i, value.to_f)
-	end
-	
-	
-	# get / set values of component by axis name
-	%w[x y z].each_with_index do |component, i|
-		# getters
-		# (same as array-style interface)
-		define_method component do
-			get_component(i)
-		end 
-		
-		# setters
-		# (use special C++ function to make sure data is written back to C++ land)
-		define_method "#{component}=" do |value|
-			set_component(i, value.to_f)
-		end 
-	end
-	
-	
-	# discards the Z component.
-	def to_cpvec2
-		return CP::Vec2.new(self.x, self.y)
-	end
+  include RubyOF::Freezable
+  
+  def to_a
+    return [self.x, self.y, self.z]
+  end
+  
+  def to_s
+    format = '%.03f'
+    x = format % self.x
+    y = format % self.y
+    z = format % self.z
+    
+    return "(#{x}, #{y}, #{z})"
+  end
+  
+  def inspect
+    super()
+  end
+  
+  
+  
+  # hide C++ level helper methods
+  private :get_component
+  private :set_component
+  
+  
+  # get / set value of a component by numerical index
+  def [](i)
+    return get_component(i)
+  end
+  
+  def []=(i, value)
+    return set_component(i, value.to_f)
+  end
+  
+  
+  # get / set values of component by axis name
+  %w[x y z].each_with_index do |component, i|
+    # getters
+    # (same as array-style interface)
+    define_method component do
+      get_component(i)
+    end 
+    
+    # setters
+    # (use special C++ function to make sure data is written back to C++ land)
+    define_method "#{component}=" do |value|
+      set_component(i, value.to_f)
+    end 
+  end
+  
+  
+  # discards the Z component.
+  def to_cpvec2
+    return CP::Vec2.new(self.x, self.y)
+  end
+  
+  # 
+  # YAML serialization interface
+  # 
+  
+  def to_yaml_type
+    "!ruby/object:#{self.class}"
+  end
+  
+  def encode_with(coder)
+    coder.represent_seq to_yaml_type, self.to_a
+  end
+  
+  def init_with(coder)
+    x,y,z = coder.seq
+    self.x = x
+    self.y = y
+    self.z = z
+    
+    initialize(x,y,z)
+  end
 end
+
 
 end
