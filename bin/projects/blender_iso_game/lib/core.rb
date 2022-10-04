6bdc13cb66b3d801b01eb59842328bcb1c6934e1
Raven <RavensKrag@gmail.com>
2022-10-04 11:47:26 -0400

    update core.rb to match new structure

--- bin/projects/blender_iso_game/lib/core.rb
+++ bin/projects/blender_iso_game/lib/core.rb
@@ -110,7 +110,7 @@ load LIB_DIR/'oit_render_pipeline.rb'
 
 load LIB_DIR/'fixed_schema_tree.rb'
 load LIB_DIR/'world.rb'
-load LIB_DIR/'vertex_animation_texture_set.rb'
+# load LIB_DIR/'vertex_animation_texture_set.rb'
 load LIB_DIR/'history.rb'
 
 
@@ -246,48 +246,9 @@ class Core
     end
     
     
-    
-    
-    
-    
-    @frame_history = History::Outer.new
-    @frame_history.bind_to_world @world
-    
-    
-    
-    
-    
     @message_history = BlenderHistory.new
+    @sync = BlenderSync.new(@message_history, @window, @world)
     
-    # @depsgraph = DependencyGraph.new
-    # ^ this was an old class for different types of entities (objects, meshes, lights, cameras) before the creation of the new vertex animation system.
-    # Keeping a reference here in a comment for historical reasons (may need to reference this code later) but generally speaking, this class should no longer be used
-      # use OIT_RenderPipeline, World, and RubyOF::Project::EntityCache instead
-    
-    @sync = BlenderSync.new(@message_history, @frame_history,
-                            @window, @world)
-    
-  end
-  
-  # run when exception is detected
-  def on_crash
-    puts "core: on_crash"
-    @crash_detected = true
-    
-    @frame_history.pause(@sync)
-    @frame_history.on_crash
-    @frame_history.update(@sync)
-    
-    
-    # TODO: handle unrecoverable exception differently than recoverable exception
-      # unrecoverable exceptions lead to program exit, rather than potential for reload. this can mean that @sync is not shut down correctly, and the FIFO remains open. need a way to detect these sorts of execptions reliably, so that the FIFO can be closed. However, during most exceptions, you want to leave the FIFO open so that Blender controls can be used for time travel, which is critical for debugging a crash.
-    
-    
-    # Don't stop sync thread on crash.
-    # Need to be able to communicate with Blender
-    # in order to control time travel
-    
-    # self.ensure()
   end
   
   # run on normal exit, before exiting program
@@ -331,9 +292,6 @@ class Core
     
     @crash_detected = false
     
-    @update_scheduler = nil
-    
-    
     @world.space.update
     
     # setup()
@@ -346,7 +304,7 @@ class Core
       @sync.reload
       # (need to re-start sync, because the IO thread is stopped in the ensure callback)
       
-      @frame_history.on_reload_code(@sync)
+      @world.on_reload_code(@sync)
     
     
     
@@ -380,12 +338,54 @@ class Core
   
   
   
+  # run when exception is detected
+  def on_crash
+    puts "core: on_crash"
+    @crash_detected = true
+    
+    @world.transport.pause(@sync)
+    @world.on_crash
+    
+    # TODO: handle unrecoverable exception differently than recoverable exception
+      # unrecoverable exceptions lead to program exit, rather than potential for reload. this can mean that @sync is not shut down correctly, and the FIFO remains open. need a way to detect these sorts of execptions reliably, so that the FIFO can be closed. However, during most exceptions, you want to leave the FIFO open so that Blender controls can be used for time travel, which is critical for debugging a crash.
+    
+    
+    # Don't stop sync thread on crash.
+    # Need to be able to communicate with Blender
+    # in order to control time travel
+    
+    # self.ensure()
+  end
+  
+  def update_while_crashed
+    self.update()
+  end
+  
+  
+  # Propagates signal from FrameHistory back up to LiveCode
+  # that the problem which caused the crash has been managed,
+  # even without loading new code.
+  def in_error_state?
+    @crash_detected
+  end
+  
+  
+  
+  
+  
+  
+  
+  
   
   def window_resized(w,h)
     # puts "generate new camera"
     # @world.camera = ViewportCamera.new
     @render_pipeline.update(@window, @world.lights)
   end
+  
+  
+  
+  
   
   
   
@@ -404,8 +404,6 @@ class Core
   
   # use a structure where Fiber does not need to be regenerated on reload
   def update
-    @crash_detected = false # reset when normal updates are called again
-    
     # @update_scheduler ||= Scheduler.new(self, :on_update, msec(16-4))
     
     # # puts ">>>>>>>> update #{RubyOF::Utils.ofGetElapsedTimeMicros}"
@@ -428,40 +426,16 @@ class Core
     # # end
     
     
-    
-    
     if @first_update
       puts "first update"
       # load_world_state()
       
       @first_update = false
-      
-      
-      # # 
-      # # jpg test
-      # # 
-      
-      # @pixels = RubyOF::Pixels.new
-      # ofLoadImage(@pixels, "/home/ravenskrag/Desktop/gem_structure/bin/projects/blender_iso_game/bin/data/hsb-cone.jpg")
-      
-      # @texture_out = RubyOF::Texture.new
-      
-      # @texture_out.wrap_mode(:vertical => :clamp_to_edge,
-      #                      :horizontal => :clamp_to_edge)
-      
-      # @texture_out.filter_mode(:min => :nearest, :mag => :nearest)
-      
-      # @texture_out.load_data(@pixels)
-      
-      
     end
-    
-    
     
     @sync.update
     
-    
-    @frame_history.update @sync do |snapshot|
+    @world.update @sync do |snapshot|
       # step every x frames
       x = 8
       
@@ -644,7 +618,15 @@ class Core
       
     end
     
-    @world.update
+    # normal update block executes while code is crashed.
+    # 
+    # The World#update block may be skipped, but the state machine etc
+    # will continue to update. If the crash is resolved,
+    # we will see that signal here, and help propagate it up to LiveCode
+    # ( signal is actually sent to LiveCode in Core#in_error_state? )
+    if @world.crash_resolved?
+      @crash_detected = false # reset when normal updates can run again
+    end
   end
   
   # methods #update and #draw are called by the C++ render loop
@@ -653,74 +635,6 @@ class Core
   # to allow for live loading - if the update / draw logic
   # is directly inside the Fiber, there's no good way to reload it
   # when the file reloads
-  
-  
-  
-  
-  def update_while_crashed
-    @crash_detected = true # set in Core#on_crash
-    
-    # puts "=== update while crashed ==="
-    
-    # pass @crash_detected flag to FrameHistory
-    @frame_history.crash_detected
-    
-    # update messages and history as necessary to try dealing with crash
-    @sync.update
-    @frame_history.update(@sync)
-      # FrameHistory will clear the @crash_detected state
-      # if you start to go back in time after a crash.
-      
-      
-      # oh wait,
-      # but need take one step back when crash is detected
-    
-    # If FrameHistory was able to use time travel to resolve the crash
-    # then clear the flag
-    if !@frame_history.crash_detected?
-      @crash_detected = false
-    end
-    
-    # puts "=== update while crashed END"
-  end
-  
-  
-  # Propagates signal from FrameHistory back up to LiveCode
-  # that the problem which caused the crash has been managed,
-  # even without loading new code.
-  def in_error_state?
-    @crash_detected
-  end
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
   
   
   
@@ -842,7 +756,7 @@ class Core
         end
         
         # while time traveling, render the trails of moving objects
-        if @frame_history.time_traveling?
+        if @world.transport.time_traveling?
           
         end
       end
@@ -865,12 +779,14 @@ class Core
         end
         
         
-        
-        @fonts[:monospace].draw_string("frame #{@frame_history.frame_index}/#{@frame_history.length-1}",
-                                         1178, 846+40)
-        
-        @fonts[:monospace].draw_string("state #{@frame_history.state}",
-                                         1178, 846)
+        @fonts[:monospace].tap do |f|
+          
+          f.draw_string("frame #{@world.transport.current_frame}/#{@world.transport.history_length-1}",
+                                           1178, 846+40)
+          
+          f.draw_string("state #{@world.transport.current_state}",
+                                           1178, 846)
+        end
         
         # @fonts[:monospace].draw_string("history size: #{}",
                                          # 400, 160)
