d47228c747069468dd9107ebcedea63de35399a6
Raven <RavensKrag@gmail.com>
2022-11-05 11:46:39 -0400

    Clean up code (reorder lines) and improve comments

--- ext/rubyOF/launcher.cpp
+++ ext/rubyOF/launcher.cpp
@@ -30,12 +30,6 @@ void rubyof_launcher_main(Rice::Object rb_app){
 	
 	cout << "-- initializing GLFW\n";
 	
-	// ofAppGlutWindow mWindow;
-	// mWindow = new ofAppGlutWindow();
-		// Glut window seems to get keyboard input just fine.
-		// It seems to break the existing implementation of Ruby-level window closing,
-		// but the Ruby-level close callback is still being called, so that's good.
-	
 	
 	// This is the easiest way to prevent a GLFW error:
 	// 
@@ -51,106 +45,108 @@ void rubyof_launcher_main(Rice::Object rb_app){
 		glfw_error = true;
 	}
 	
-	
-	
-	
-	// // Even if an error has been detected, still proceed with
-	// // the full initialization. This way, openFrameworks has
-	// // a chance to output it's error message.
-	// cout << "-- creating GLFW window\n";
-	// mWindow = new ofAppGLFWWindow();
-	
-	// cout << "-- configuring GLFW window\n";
-	// ofSetupOpenGL(mWindow, width,height,OF_WINDOW); // <-------- setup the GL context
-	
+	// ofAppGLFWWindow.cpp : ofAppGLFWWindow::setup(const ofGLFWWindowSettings & _settings)
+		// if(!glfwInit( )){
+		// 	ofLogError("ofAppGLFWWindow") << "couldn't init GLFW";
+		// 	return;
+		// }
 	
 	// Even if an error has been detected, still proceed with
 	// the full initialization. This way, openFrameworks has
 	// a chance to output it's error message.
+	
+	
+	
+	
+	
+	
+	// Documentation says the correct way to create a window is using ofCreateWindow()
+	// src: https://openframeworks.cc/documentation/application/
+	// Need to use some variant of that code in order to enable multi-window setups,
+	// porting to mobile, etc.
+		// Some versions of this code break mouse events.
+		// Seems ok on v0.11		[ as of 2022.11.05 ]
+	
+	
+	// ofAppRunner.cpp   : ofSetupOpenGL
+	// ofAppRunner.cpp   : ofCreateWindow
+		// ofInit();
+		// mainLoop()->createWindow(settings);
+	
+	// ofMainLoop.cpp:65-70 : ofMainLoop::createWindow(const ofWindowSettings & settings)
+		// [ Preprocessor conditionals omitted for brevity. ]
+		// [ They define other types of windows. ]
+		// 65     shared_ptr<ofAppGLFWWindow> window = std::make_shared<ofAppGLFWWindow>();
+		// ...
+		// 68     addWindow(window);
+		// 69     window->setup(settings);
+		// 70     return window;
+	
+	// ofMainLoop.h
+		// std::shared_ptr<ofAppBaseWindow> createWindow(const ofWindowSettings & settings);
+		// template<typename Window>
+		// void addWindow(std::shared_ptr<Window> window){
+		// 	allowMultiWindow = Window::allowsMultiWindow();
+		// 	if(Window::doesLoop()){
+		// 	    windowLoop = Window::loop;
+		// 	}
+		// 	if(Window::needsPolling()){
+		// 		windowPollEvents = Window::pollEvents;
+		// 	}
+		// 	if(!allowMultiWindow){
+		// 	    windowsApps.clear();
+		// 	}
+		// 	windowsApps[window] = std::shared_ptr<ofBaseApp>();
+		// 	currentWindow = window;
+		// 	ofAddListener(window->events().keyPressed,this,&ofMainLoop::keyPressed);
+		// }
+	
+	
+	
+	
+	
+	// window is the drawing context
+	// app is the thing that holds all the update and render logic
+	
+	// oF defines different types of windows that can be used, and I want to try the GLFW one
+	// (GLFW window appears to be the default, actually)
+	
+	
+	// ofAppGlutWindow mWindow;
+	// mWindow = new ofAppGlutWindow();
+		// Glut window seems to get keyboard input just fine.
+		// It seems to break the existing implementation of Ruby-level window closing,
+		// but the Ruby-level close callback is still being called, so that's good.
+	
 	cout << "-- creating GLFW window\n";
 	mWindow = shared_ptr<ofAppGLFWWindow>(new ofAppGLFWWindow());
 	
-	
 	cout << "-- configuring GLFW window\n";
 	// ofSetupOpenGL(mWindow, width,height,OF_WINDOW); // <-------- setup the GL context
+		//                                  ^^^^^^^^^
+		// can be OF_WINDOW or OF_FULLSCREEN
+	ofInit();
+	auto settings = mWindow->getSettings();
+		settings.windowMode = OF_WINDOW;
 		
-		// shared_ptr<ofAppGLFWWindow> windowPtr (mWindow);
+		// set window size
+		int width  = from_ruby<int>(rb_app.call("width"));
+		int height = from_ruby<int>(rb_app.call("height"));
 		
-		ofInit();
-		auto settings = mWindow->getSettings();
-			settings.windowMode = OF_WINDOW;
-			
-			// set window size
-			int width  = from_ruby<int>(rb_app.call("width"));
-			int height = from_ruby<int>(rb_app.call("height"));
-			
-			settings.setSize(width,height);
-			
-			// set opengl version
-			// default to opengl version 3.2 (default specified in rb_app.rb)
-			int opengl_version_major = from_ruby<int>(rb_app.call("opengl_version_major"));
-			int opengl_version_minor = from_ruby<int>(rb_app.call("opengl_version_minor"));
-			
-			settings.setGLVersion(opengl_version_major, opengl_version_minor);
-			
-			// ^ simply setting the GL version seems to break mouse events? why?
-			// After extensive testing, it appears to be an interaction with imgui.
-			// I don't understand how that works... but ok.
-		ofGetMainLoop()->addWindow(mWindow);
-		mWindow->setup(settings);
-	
-	
-	
-	
-	
-	
-	// // Even if an error has been detected, still proceed with
-	// // the full initialization. This way, openFrameworks has
-	// // a chance to output it's error message.
-	// cout << "-- creating GLFW window\n";
-	// mWindow = new ofAppGLFWWindow();
-	
-	// cout << "-- configuring GLFW window\n";
-	// ofGLFWWindowSettings settings = mWindow->getSettings();
-	// 	settings.setGLVersion(3,2);
-	// 	settings.setSize(width, height);
-	// 	settings.windowMode = OF_WINDOW;
-	// ofGetMainLoop()->addWindow(shared_ptr<ofAppGLFWWindow>(mWindow)); // TODO: convert to shared pointer
-	// mWindow->setup(settings);
-	// // ofSetupOpenGL(mWindow, width,height,OF_WINDOW); // <-------- setup the GL context
-	
-	
-	// ofInit();
-	
-	
-	
-	// ofCreateWindow(settings) is define in the file below:
-	// ext/openFrameworks/libs/openFrameworks/app/ofAppRunner.cpp
-			// ofCreateWindow
-				// mainLoop()->createWindow(settings)
-	// ext/openFrameworks/libs/openFrameworks/app/ofMainLoop.cpp
-	// 
-	// 
-	// ext/openFrameworks/libs/openFrameworks/app/ofAppGLFWWindow.cpp
-	
-	
-	
-	// the correct way to set up a window is using ofCreateWindow()
-	// src: https://openframeworks.cc/documentation/application/
-	// but currently, that breaks mouse events (not sure why)
-	// Maybe this problem will fix itself when I upgrade oF to the stable version? but I really don't know.
-	
-	// want to eventually transition to using ofCreateWindow, because that will enable multi-window setups, and porting to mobile, etc... but for right now let's not do that.
-	
-	// cout << "-- creating GLFW window\n";
-	// ofGLFWWindowSettings settings;
-	// 	settings.setGLVersion(3,2);
-	// 	// settings.setSize(width, height);
-	// mWindow = ofCreateWindow(settings);
-	
-	// ofSetWindowShape(width, height);
-	
-	
+		settings.setSize(width,height);
+		
+		// set opengl version
+		// default to opengl version 3.2 (default specified in rb_app.rb)
+		int opengl_version_major = from_ruby<int>(rb_app.call("opengl_version_major"));
+		int opengl_version_minor = from_ruby<int>(rb_app.call("opengl_version_minor"));
+		
+		settings.setGLVersion(opengl_version_major, opengl_version_minor);
+		
+		// ^ simply setting the GL version seems to break mouse events? why?
+		// After extensive testing, it appears to be an interaction with imgui.
+		// I don't understand how that works... but ok.
+	ofGetMainLoop()->addWindow(mWindow);
+	mWindow->setup(settings);
 	
 	// At this point, the error message is out, and the error flag is set.
 	// If there is an error, bail out here,
@@ -158,8 +154,6 @@ void rubyof_launcher_main(Rice::Object rb_app){
 	if(glfw_error){
 		throw Rice::Exception(rb_eRuntimeError, "GLFW initialization error.");
 	}
-	
-	
 	
 	cout << "-- creating openFrameworks app...\n";
 	
@@ -188,33 +182,13 @@ void rubyof_launcher_main(Rice::Object rb_app){
 	
 	cout << "-- binding complete!\n";
 	
-	// window is the drawing context
-	// app is the thing that holds all the update and render logic
-	
-	// oF defines different types of windows that can be used, and I want to try the GLFW one
-	// (GLFW window appears to be the default, actually)
-	
-	
-	
-	// ofAppRunner.cpp   : ofSetupOpenGL
-	// ofAppRunner.cpp   : ofCreateWindow
-		// ofInit();
-		// mainLoop()->createWindow(settings);
-	
-	// ofMainLoop.cpp:43 : ofMainLoop::createWindow
-		// shared_ptr<ofAppGLFWWindow> window =
-		// shared_ptr<ofAppGLFWWindow>(new ofAppGLFWWindow());
-	// and various other types of windows
-	
-	
 	
 	cout << "-- run the ofApp\n";
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
-	ofRunApp(mApp);
 	
-	
+		// this kicks off the running of my app
+		ofRunApp(mApp);
+		
+	cout << "-- ofApp has finished\n";
 	
 	
 	cout << "-- clean up memory\n";
